905d3139fe879aafb4805529bd3ad7ccfd67945b
Pedro Ferreira <pedrodsf21@gmail.com>
2024-01-18 17:05:20 +0000

    Add utils

---
+++ meao/utils.py
@@ -0,0 +1,23 @@
+import json
+
+import cherrypy
+
+
+def jsonify_error(status, message, traceback, version):
+    """JSONify all CherryPy error responses (created by raising the
+    cherrypy.HTTPError exception)
+    """
+
+    cherrypy.response.headers["Content-Type"] = "application/json"
+    response_body = json.dumps(
+        {
+            "error": {
+                "http_status": status,
+                "message": message,
+            }
+        }
+    )
+
+    cherrypy.response.status = status
+
+    return response_body
