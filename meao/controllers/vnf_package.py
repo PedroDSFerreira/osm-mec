b847a08ad19d2bbcaefe4b12899a3b8a5cbf689d
Pedro Ferreira <pedrodsf21@gmail.com>
2024-01-24 18:56:45 +0000

    Add new vnf package request

--- meao/controllers/vnf_package.py
+++ meao/controllers/vnf_package.py
@@ -9,20 +9,34 @@ class VnfPackageController:
         /vnf_packages (GET)
         """
 
-        print(f"{cherrypy.config.get('OSM_HOST')}/osm/vnfpkgm/v1/vnf_packages")
         token = cherrypy.request.headers["Authorization"]
-
         response = requests.get(
             f"{cherrypy.config.get('OSM_HOST')}/osm/vnfpkgm/v1/vnf_packages",
             headers={"Authorization": token, "Accept": "application/json"},
         )
+
+        cherrypy.response.status = response.status_code
+
         return response.json()
 
-    # @cherrypy.tools.json_out()
-    # def new_package(self):
-    #     """
-    #     /vnf_packages (POST)
-    #     """
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def new_package(self):
+        """
+        /vnf_packages (POST)
+        """
+
+        token = cherrypy.request.headers["Authorization"]
+        data = cherrypy.request.json
+        response = requests.post(
+            f"{cherrypy.config.get('OSM_HOST')}/osm/vnfpkgm/v1/vnf_packages",
+            headers={"Authorization": token, "Accept": "application/json"},
+            json=data,
+        )
+
+        cherrypy.response.status = response.status_code
+
+        return response.json()
 
     @cherrypy.tools.json_out()
     def get_package(self, package_id):
@@ -31,9 +45,11 @@ class VnfPackageController:
         """
 
         token = cherrypy.request.headers["Authorization"]
-
         response = requests.get(
             f"{cherrypy.config.get('OSM_HOST')}/osm/vnfpkgm/v1/vnf_packages/{package_id}",
             headers={"Authorization": token, "Accept": "application/json"},
         )
+
+        cherrypy.response.status = response.status_code
+
         return response.json()
