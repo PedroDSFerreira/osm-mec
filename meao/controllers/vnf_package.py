69a435907e82bc2db834a2a1fb0e9508749becfe
Pedro Ferreira <pedrodsf21@gmail.com>
2024-02-11 11:57:51 +0000

    Update vnf package controller

--- meao/controllers/vnf_package.py
+++ meao/controllers/vnf_package.py
@@ -1,61 +1,44 @@
 import cherrypy
-from osmclient import client
 from osmclient.common.exceptions import ClientException
-import json
-import requests
-import os
-import io, sys
-from utils import save_file
+from utils import CaptureIO, delete_file, save_file
 
-
-#myclient = client.Client(host="10.255.41.31", sol005=True)
 
 class VnfPackageController:
     def __init__(self, client):
-        self.configuration_file_path="vnf_package_configurations"
-        self.myclient = client
-
+        self.descriptors_dir = "vnf_package_descriptors"
+        self.client = client
 
     @cherrypy.tools.json_out()
     def get_vnf_packages_content(self):
         """
         /vnf_packages_content (GET)
         """
-        return self.myclient.vnfd.list()
-    
-        
+        return self.client.vnfd.list()
+
     @cherrypy.tools.json_out()
     def new_vnf_package_content(self, file):
         """
         /vnf_packages_content (POST)
         """
-    
-        file_path = save_file(self.configuration_file_path, file)
+
+        file_path = save_file(self.descriptors_dir, file)
 
         try:
-            #create function returns null, so we need to capture the output from stdout stream
-            backup = sys.stdout
-            sys.stdout = io.StringIO()
-            self.myclient.vnfd.create(filename=file_path) 
-            out = sys.stdout.getvalue()
-            sys.stdout.close()
-            sys.stdout = backup
+            with CaptureIO() as out:
+                self.client.vnfd.create(filename=file_path)
+
             return {"response": out}
         except ClientException as e:
             return {"error": str(e)}
         finally:
-            # Delete configuration file after use
-            if os.path.exists(file_path):
-                os.remove(file_path)    
-
+            delete_file(file_path)
 
     @cherrypy.tools.json_out()
     def get_vnf_package_content(self, vnf_package_id):
         """
         /vnf_packages/{vnfPkgId}/package_content (GET)
         """
-        return self.myclient.vnfd.get(name=vnf_package_id)
-        
+        return self.client.vnfd.get(name=vnf_package_id)
 
     @cherrypy.tools.json_out()
     def update_vnf_package_content(self, vnf_package_id, file):
@@ -63,29 +46,25 @@ class VnfPackageController:
         /vnf_packages/{vnfPkgId}/package_content (PUT)
         """
 
-        file_path = save_file(self.configuration_file_path, file)
+        file_path = save_file(self.descriptors_dir, file)
 
         try:
-            return self.myclient.vnfd.update(name=vnf_package_id, filename=file_path)
+            return self.client.vnfd.update(name=vnf_package_id, filename=file_path)
         except ClientException as e:
             return {"error": str(e)}
         finally:
-            # Delete configuration file after use
-            if os.path.exists(file_path):
-                os.remove(file_path)  
-
+            delete_file(file_path)
 
     @cherrypy.tools.json_out()
     def get_vnfd(self, vnf_package_id):
         """
         /vnf_packages/{vnfPkgId}/vnfd (GET)
         """
-        return self.myclient.vnfd.get(name=vnf_package_id)
-        
+        return self.client.vnfd.get(name=vnf_package_id)
 
     @cherrypy.tools.json_out()
     def delete_vnf_package_content(self, package_content_id):
         """
         /vnf_packages_content/{packageContentId} (DELETE)
         """
-        return self.myclient.vnfd.delete(name=package_content_id)
+        return self.client.vnfd.delete(name=package_content_id)
