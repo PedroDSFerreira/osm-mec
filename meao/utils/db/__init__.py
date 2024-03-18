6ec4824c046eb760ecbe05096f05e0ef8ada1b4a
Pedro Ferreira <pedrodsf21@gmail.com>
2024-03-18 18:37:34 +0000

    Update utils

---
+++ meao/utils/db/__init__.py
@@ -0,0 +1,2 @@
+from .db_utils import DB
+from .db_init import DatabaseInitializer
---
+++ meao/utils/db/db_utils.py
@@ -0,0 +1,39 @@
+from pymongo import MongoClient
+import os
+from bson import ObjectId
+
+client = MongoClient('mongodb://mongo:27017/',
+                                     username=os.getenv('MONGO_USER'),
+                                     password=os.getenv('MONGO_PASSWORD'))
+
+db = client['db']
+
+class DB:
+    @staticmethod
+    def _get(id, collection):
+        return db[collection].find_one({'_id': ObjectId(id)})
+
+    @staticmethod
+    def _find(collection, filter=None):
+        return db[collection].find_one(filter)
+
+    @staticmethod
+    def _list(collection, filter=None):
+        return list(db[collection].find(filter))
+
+    @staticmethod
+    def _add(collection, data):
+        id = db[collection].insert_one(data).inserted_id
+        return str(id)
+
+    @staticmethod
+    def _update(id, collection, data):
+        db[collection].update_one({'_id': ObjectId(id)}, {'$set': data})
+
+    @staticmethod
+    def _delete(id, collection):
+        db[collection].delete_one({'_id': ObjectId(id)})
+
+    @staticmethod
+    def _exists(id, collection):
+        return db[collection].find_one({'_id': ObjectId(id)}) is not None
