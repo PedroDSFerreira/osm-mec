0ef6bddbce8663b894df75650d5138ecb73e9356
Pedro Ferreira <pedrodsf21@gmail.com>
2024-03-18 18:38:16 +0000

    Add kafka producer

--- meao/main.py
+++ meao/main.py
@@ -3,12 +3,17 @@ import logging
 import os
 
 from callbacks import load_callback_functions
-from kafka import KafkaConsumer
+from kafka import KafkaConsumer, KafkaProducer
+from utils.db import DatabaseInitializer
 
 logging.basicConfig(level=logging.INFO)
 
 
 def main():
+    producer = KafkaProducer(
+        bootstrap_servers=os.getenv("KAFKA_BOOTSTRAP_SERVERS"),
+        value_serializer=lambda v: json.dumps(v).encode("utf-8"),
+    )
     consumer = KafkaConsumer(
         bootstrap_servers=os.getenv("KAFKA_BOOTSTRAP_SERVERS"),
         group_id="group",
@@ -29,7 +34,9 @@ def main():
             topic = message.topic
             if topic in callbacks:
                 callback_function = callbacks[topic]
-                callback_function(message.value)
+                response = callback_function(message.value)
+                producer.send("responses", value=response)
+                logging.info(f"Sent response: {response}")
 
     except Exception as e:
         logging.error(f"An error occurred: {e}")
@@ -39,4 +46,5 @@ def main():
 
 
 if __name__ == "__main__":
+    DatabaseInitializer.initialize_database()
     main()
